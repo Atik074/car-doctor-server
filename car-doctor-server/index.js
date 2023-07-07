4a6eb5ab23c08d80849560ec7e2b2cbfa2e24485
Atik074 <atiksmats#@gmail.com>
2023-07-07 21:12:06 +0600

    some data readed

--- car-doctor-server/index.js
+++ car-doctor-server/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000
 const cors = require('cors')
@@ -8,7 +8,6 @@ const cors = require('cors')
 // midleware
 app.use(cors())
 app.use(express.json())
-
 
 
 
@@ -30,10 +29,58 @@ async function run() {
     
     const servicesCollection = client.db("carDoctor").collection("services");
 
+    // carDoctor ar undare  manush j sob service order korbe sei sob data mongodb te store kore rakhte hbe.tai notun collection toiry korte hobe.
+    const bookingCollection = client.db('carDoctor').collection('bookings')
+
+
     app.get('/services', async(req, res)=>{
       const cursor = servicesCollection.find();
       const result = await cursor.toArray()
        res.send(result)
+    })
+
+    app.get('/services/:id' , async(req, res)=>{
+      const id = req.params.id 
+      const query = {_id : new ObjectId(id)}
+   const options = {
+     // Include only the `title` and `imdb` fields in the returned document
+        projection: {  title: 1, price:1 ,service_id:1 , img:1},
+      };
+
+      const result = await servicesCollection.findOne(query,options)
+      res.send(result)
+    })
+
+    // bookings ........
+
+  //   all data  is not needed and specific one data is not needed. some  data  will  be needed .for this :
+  app.get('/bookings' , async(req,res)=>{
+    console.log(req.query.email)
+    let query = {};  
+    if(req.query?.email){
+      query = {email : req.query.email}
+    }
+    const result = await bookingCollection.find().toArray()
+      res.send(result) 
+  })
+
+
+
+  // booking data created
+    app.post('/bookings' , async(req, res)=>{
+       const booking = req.body 
+       console.log(booking)
+       const doc ={
+        CustomerName :booking. name ,
+              service:booking.title,
+             service_id:booking._id ,
+            number :booking.number ,
+            date :booking.date ,
+            price :booking.price
+       }
+       const result = await bookingCollection.insertOne(doc)
+       res.send(result)
+
     })
 
 
@@ -61,5 +108,5 @@ app.get("/", (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`port runninr on ${port}`)
+  console.log(`port running  on ${port}`)
 })
